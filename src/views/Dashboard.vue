6ddcee56bed49239d1fbed42845833786c856be9
dmonrabal <dmonrabal@gmail.com>
2023-11-01 00:50:18 +0100

    test after commit

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -336,7 +336,7 @@
       <v-dialog v-model="dialogDelete" max-width="500px">
         <v-card>
           <v-card-title class="text-h5"
-            >¿Estás seguro de eliminar este elemento?</v-card-title
+            >¿Estás seguro de eliminar este elemento test?</v-card-title
           >
           <v-card-actions>
             <v-spacer></v-spacer>
